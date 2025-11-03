92de6669cc7c5644009342cd38f964957f5fb069
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-03 14:46:53 -0300

    Atualizar o filme.js

--- src/models/filme.js
+++ src/models/filme.js
@@ -1,33 +1,34 @@
 // ARQUIVO: src/models/filme.js
-// (Modelo para Filme - Fundo preto, poster e titulo grande)
+// (CORRIGIDO: Titulo 100% centralizado na area preta)
 
 const Jimp = require('jimp');
-// ATUALIZADO: so importa o poster
 const { drawPoster } = require('../drawing/poster.js'); 
 
 async function draw(image, anime, fonts, consts) {
   const { largura, altura, padding } = consts;
   const { fontFilme } = fonts; // Usa a nova fonte 108px
-  
-  // 1. Fundo Preto (O 'image' ja e preto por padrao)
-  
+
+  // 1. Fundo Preto (o 'image' ja e preto por padrao)
+
   // 2. Poster
   const posterWidth = await drawPoster(image, anime.coverImage.large, largura, altura, padding);
-  
+
   // 3. Area de Texto
+  // A area de texto e o espaco do inicio (0) + padding ate o poster
+  const textoAreaX = padding;
   const textoAreaLargura = largura - posterWidth - (padding * 2);
   const titulo = anime.title.romaji || anime.title.english || "Titulo Desconhecido";
-  
+
   // 4. Desenha o Titulo
-  // (Centraliza o texto verticalmente na area livre)
+  // --- *** A MUDANCA ESTA AQUI *** ---
   image.print(
     fontFilme,
-    padding, // X
-    0,       // Y
+    textoAreaX, // X (Onde a area de texto comeca)
+    0,          // Y (Topo da imagem)
     {
       text: titulo,
-      alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT,
-      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER, // <--- CORRIGIDO
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE  // (Ja estava correto)
     },
     textoAreaLargura, // Max Largura
     altura          // Max Altura
