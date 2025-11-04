f8f1d1ee22dd98e7efea28449e9e28244c96b293
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-04 13:37:47 -0300

    Atualizar o text.js

--- src/drawing/text.js
+++ src/drawing/text.js
@@ -1,28 +1,29 @@
 // ARQUIVO: src/drawing/text.js
-// (ATUALIZADO: Nao desenha mais o Estudio)
+// (ATUALIZADO: Usa as novas fontes 'fontTituloTV' e 'fontInfoTV')
 
 const Jimp = require('jimp');
 const { traduzirTemporada } = require('../utils.js'); 
 
 // --- 3. Desenha os Textos Principais (Info e Titulo) ---
 async function drawText(image, anime, fonts, padding, textAreaWidth) {
-  const { fontTitulo, fontInfo } = fonts;
+  // --- *** MUDANCA: Pega as fontes corretas do objeto *** ---
+  const { fontTituloTV, fontInfoTV } = fonts;
   let currentTextY = padding;
 
   const temporada = traduzirTemporada(anime.season);
   const episodios = anime.episodes || '??';
   
+  // Usa fontInfoTV (Roboto Bold 34)
   const infoTopo = `${temporada} ${anime.seasonYear} - ${episodios} EPISODIOS`;
-  image.print(fontInfo, padding, currentTextY, infoTopo, textAreaWidth);
-  currentTextY += Jimp.measureTextHeight(fontInfo, infoTopo, textAreaWidth) + 10;
+  image.print(fontInfoTV, padding, currentTextY, infoTopo, textAreaWidth);
+  currentTextY += Jimp.measureTextHeight(fontInfoTV, infoTopo, textAreaWidth) + 10;
   
+  // Usa fontTituloTV (Boogaloo 47)
   const titulo = anime.title.romaji || anime.title.english || "Titulo Desconhecido";
-  image.print(fontTitulo, padding, currentTextY, titulo, textAreaWidth);
-  currentTextY += Jimp.measureTextHeight(fontTitulo, titulo, textAreaWidth) + 20;
+  image.print(fontTituloTV, padding, currentTextY, titulo, textAreaWidth);
+  currentTextY += Jimp.measureTextHeight(fontTituloTV, titulo, textAreaWidth) + 20;
   
-  // A parte do "Estudio" foi removida daqui
-  
-  return currentTextY; // Retorna o Y (nao e mais usado, mas e bom manter)
+  return currentTextY;
 }
 
 module.exports = { drawText };
