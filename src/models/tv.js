e76a2f958ce3e3aade53a46f50c12aab4c63c56d
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-03 22:18:10 -0300

    Atualizar o tv.js

--- src/models/tv.js
+++ src/models/tv.js
@@ -1,8 +1,6 @@
 // ARQUIVO: src/models/tv.js
-// (CORRIGIDO: Importando os 5 modulos de desenho corretamente)
+// (ATUALIZADO: Ordem de desenho corrigida)
 
-// --- *** A CORRECAO ESTA AQUI *** ---
-// Em vez de importar 1 modulo, importamos os 5 arquivos de /drawing/
 const { drawBackground } = require('../drawing/background.js');
 const { drawPoster } = require('../drawing/poster.js');
 const { drawText } = require('../drawing/text.js');
@@ -11,24 +9,28 @@ const { drawClassification } = require('../drawing/classification.js');
 
 async function draw(image, anime, fonts, consts) {
   const { largura, altura, padding } = consts;
-  
+
   // 1. Fundo
   await drawBackground(image, anime.bannerImage, largura, altura);
-  
+
   // 2. Poster
   const posterWidth = await drawPoster(image, anime.coverImage.large, largura, altura, padding);
-  
+
   // 3. Area de Texto
   const textoAreaLargura = largura - posterWidth - (padding * 2);
+
+  // 4. Textos (Info, Titulo, Estudio) - CONTINUAM NO TOPO
+  // (Nao precisamos mais do 'proximoY' aqui)
+  await drawText(image, anime, fonts, padding, textoAreaLargura);
+
+  // --- *** MUDANCA DE POSICAO *** ---
   
-  // 4. Textos
-  const proximoY = await drawText(image, anime, fonts, padding, textoAreaLargura);
-  
-  // 5. Tags
-  await drawTags(image, anime, fonts, padding, textoAreaLargura, proximoY);
-  
-  // 6. Classificacao
+  // 5. Classificacao (desenhada primeiro no fundo)
   await drawClassification(image, anime, padding, altura);
+  
+  // 6. Tags (desenhadas logo acima da classificacao)
+  // (Agora passamos 'altura' em vez de 'proximoY')
+  await drawTags(image, anime, fonts, padding, textoAreaLargura, altura);
 }
 
 module.exports = { draw };
