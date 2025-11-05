c9b8343451f51a6f574dadf3b4ba0fbf12835f7f
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-04 22:57:41 -0300

    Atualizar o tv.js

--- src/models/tv.js
+++ src/models/tv.js
@@ -1,25 +1,29 @@
 // ARQUIVO: src/models/tv.js
-// (ATUALIZADO: Chama o novo 'drawBottomBar')
+// (ATUALIZADO: Ordem de desenho invertida)
 
 const { drawBackground } = require('../drawing/background.js');
 const { drawPoster } = require('../drawing/poster.js');
 const { drawText } = require('../drawing/text.js');
-// --- *** MUDANCA: Importa o novo 'bottomBar.js' *** ---
 const { drawBottomBar } = require('../drawing/bottomBar.js');
 
 async function draw(image, anime, fonts, consts) {
   const { largura, altura, padding } = consts;
 
-  await drawBackground(image, anime.bannerImage, largura, altura);
+  // --- *** MUDANCA DE ORDEM *** ---
+  // 1. Desenha o Poster PRIMEIRO para saber a largura dele
   const posterWidth = await drawPoster(image, anime.coverImage.large, largura, altura, padding);
+  
+  // 2. Desenha o Fundo AGORA, passando o posterWidth que acabamos de pegar
+  await drawBackground(image, anime.bannerImage, largura, altura, posterWidth);
+  // --- *** FIM DA MUDANCA *** ---
+
+  // 3. Calcula a area de texto (restante)
   const textoAreaLargura = largura - posterWidth - (padding * 2);
 
-  // --- ORDEM DE DESENHO ATUALIZADA ---
-  
-  // 1. Textos (Info, Titulo) - Ficam no TOPO
+  // 4. Textos (Info, Titulo) - Ficam no TOPO
   await drawText(image, anime, fonts, padding, textoAreaLargura);
-  
-  // 2. Barra Inferior (Estudio, 6 Tags, Classificacao)
+
+  // 5. Barra Inferior (Estudio, Tags, Classificacao)
   await drawBottomBar(image, anime, fonts, padding, textoAreaLargura, altura);
 }
 
