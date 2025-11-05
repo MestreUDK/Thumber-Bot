fd1193f9b2634d5faed0a64a5780a6ef74cf4428
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-04 23:53:07 -0300

    Atualizar o ona.js

--- src/models/ona.js
+++ src/models/ona.js
@@ -1,5 +1,5 @@
 // ARQUIVO: src/models/ona.js
-// (VERIFICADO: Passando a altura total da capa (720) para o fundo)
+// (ATUALIZADO: Agora preserva o 'seasonYear' original)
 
 const { drawBackground } = require('../drawing/background.js');
 const { drawPoster } = require('../drawing/poster.js');
@@ -11,17 +11,21 @@ async function draw(image, anime, fonts, consts) {
 
   const animeONA = JSON.parse(JSON.stringify(anime));
   animeONA.season = 'ONA';
-  animeONA.seasonYear = ''; 
+  
+  // --- *** MUDANÇA AQUI *** ---
+  // A linha "animeONA.seasonYear = '';" foi REMOVIDA.
+  // Agora, o 'seasonYear' original (ex: 2012) sera passado para o 'drawText'.
 
   // 1. Desenha o Poster
   const posterWidth = await drawPoster(image, animeONA.coverImage.large, largura, altura, padding);
-  
+
   // 2. Desenha o Fundo, passando a 'altura' (720px) para ele
   await drawBackground(image, animeONA.bannerImage, largura, altura, posterWidth); // <-- 'altura' é 720
-  
+
   const textoAreaLargura = largura - posterWidth - (padding * 2);
 
   // 4. Textos (Info, Titulo)
+  // Agora 'animeONA' sera passado com o 'seasonYear' correto
   await drawText(image, animeONA, fonts, padding, textoAreaLargura);
 
   // 5. Barra Inferior
