37cb87379df1b7aabc6b1cccc0e5844ce2b88198
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-11-04 23:29:28 -0300

    Atualizar o text.js

--- src/drawing/text.js
+++ src/drawing/text.js
@@ -1,28 +1,51 @@
 // ARQUIVO: src/drawing/text.js
-// (ATUALIZADO: Usa as novas fontes 'fontTituloTV' e 'fontInfoTV')
+// (ATUALIZADO: Textos alinhados a direita)
 
 const Jimp = require('jimp');
 const { traduzirTemporada } = require('../utils.js'); 
 
 // --- 3. Desenha os Textos Principais (Info e Titulo) ---
 async function drawText(image, anime, fonts, padding, textAreaWidth) {
-  // --- *** MUDANCA: Pega as fontes corretas do objeto *** ---
   const { fontTituloTV, fontInfoTV } = fonts;
   let currentTextY = padding;
 
   const temporada = traduzirTemporada(anime.season);
   const episodios = anime.episodes || '??';
-  
-  // Usa fontInfoTV (Roboto Bold 34)
   const infoTopo = `${temporada} ${anime.seasonYear} • ${episodios} EPISÓDIOS`;
-  image.print(fontInfoTV, padding, currentTextY, infoTopo, textAreaWidth);
+
+  // --- *** MUDANÇA AQUI (INFO) *** ---
+  // Trocamos a chamada simples por uma chamada com objeto de opções
+  // para definir o 'alignmentX' como 'RIGHT' (Direita).
+  image.print(
+    fontInfoTV,
+    padding, // O X onde a area de texto comeca
+    currentTextY,
+    {
+      text: infoTopo,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT // Alinhamento a Direita
+    },
+    textAreaWidth // A largura maxima da area de texto
+  );
   currentTextY += Jimp.measureTextHeight(fontInfoTV, infoTopo, textAreaWidth) + 10;
-  
-  // Usa fontTituloTV (Boogaloo 47)
+  // --- FIM DA MUDANCA ---
+
   const titulo = anime.title.romaji || anime.title.english || "Titulo Desconhecido";
-  image.print(fontTituloTV, padding, currentTextY, titulo, textAreaWidth);
+
+  // --- *** MUDANÇA AQUI (TITULO) *** ---
+  // Fazemos a mesma coisa para o Titulo.
+  image.print(
+    fontTituloTV,
+    padding, // O X onde a area de texto comeca
+    currentTextY,
+    {
+      text: titulo,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT // Alinhamento a Direita
+    },
+    textAreaWidth // A largura maxima da area de texto
+  );
   currentTextY += Jimp.measureTextHeight(fontTituloTV, titulo, textAreaWidth) + 20;
-  
+  // --- FIM DA MUDANCA ---
+
   return currentTextY;
 }
 
