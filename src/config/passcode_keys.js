e726149f487e45dfea4f7aaeaa7d4f668f64fc8d
MestreUDK <117117483+MestreUDK@users.noreply.github.com>
2025-12-11 11:23:52 -0300

    Criar src/config/passcode_keys.js

---
+++ src/config/passcode_keys.js
@@ -0,0 +1,16 @@
+// Arquivo: src/config/passcode_keys.js
+// Mapeamento de chaves para minificação do Passcode
+const KEY_MAP = {
+  'title': 't', 'romaji': 'r', 'english': 'e',
+  'season': 's', 'seasonYear': 'y', 'episodes': 'ep',
+  'studios': 'st', 'nodes': 'n', 'name': 'nm',
+  'genres': 'g', 'averageScore': 'sc', 'format': 'fmt',
+  'coverImage': 'ci', 'large': 'l', 'bannerImage': 'bi',
+  'classificacaoManual': 'cm', 'infoManual': 'im', 'layout': 'la',
+  'description': 'desc', 'status': 'stat',
+  'startDate': 'sd', 'endDate': 'ed', 'year': 'yr',
+  'abrev': 'abr', 'audio': 'aud', 'seasonNum': 'sn',
+  'partNum': 'pn', 'seasonName': 'snm'
+};
+
+module.exports = KEY_MAP;
